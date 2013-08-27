3717aa45ca9b2f8f75a9838084b472dd60ed1678
David Stack <davidhstack@gmail.com>
2013-08-26 18:42:47 -0700

    Created quick installation for entire module
    Should work on Windows 7 and above

---
+++ install.bat
@@ -0,0 +1,16 @@
+@ ECHO OFF
+
+:: Set Variables
+SET APSIMDIR="C:\Program Files (x86)\Apsim74-r2286\Model"
+SET PYTHONDIR="C:\Python27\Lib\site-packages"
+SET APSIMPACKAGE="apsimRegions"
+:: Copy files to Apsim model directory
+ROBOCOPY scripts\apsimRun %APSIMDIR%
+
+:: Create a directory with the same name as the package and
+:: copy all apsimRegions files to Python site-packages directory
+MD %PYTHONDIR%\\%APSIMPACKAGE%
+ROBOCOPY %APSIMPACKAGE% %PYTHONDIR%\\%APSIMPACKAGE% /S
+
+ECHO Installation complete!
+PAUSE
